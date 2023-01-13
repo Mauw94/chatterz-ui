14d96c62591dedaed48d62fd2f28cdf6d62a215c
Maurits Seelen <mauritsseelen@hotmail.com>
2023-01-13 12:02:25 +0100

    correct api for prod env

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -3,7 +3,7 @@ import { environment } from "src/environments/environment"
 export class Const {
     public static getBaseUrl(): string {
         if (environment.production)
-            return "https://localhost:7291/" // TODO: azure url here
+            return "https://chatterz.azurewebsites.net/" // TODO: azure url here
         else
             return "https://localhost:7291/"
     }
